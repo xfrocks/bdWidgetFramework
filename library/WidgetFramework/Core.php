xfrocks/bdWidgetFramework

commit e0cc869a90409a4e2a86755b113f228965ac467d

streamlined memory usage by avoiding to load renderer when it’s not neccessary

library/WidgetFramework/Core.php
@@ -76,7 +76,7 @@ class WidgetFramework_Core
 		if (defined('WIDGET_FRAMEWORK_LOADED'))
 			return false;
 
-		$globalWidgets = $this->_getModelWidget()->getGlobalWidgets();
+		$globalWidgets = $this->_getModelWidget()->getGlobalWidgets(true, false);
 		$this->_getModelWidget()->reverseNegativeDisplayOrderWidgets($globalWidgets);
 		$this->addWidgets($globalWidgets);
 

library/WidgetFramework/Model/Widget.php
@@ -87,10 +87,11 @@ class WidgetFramework_Model_Widget extends XenForo_Model
 					", 'widget_id');
 		}
 
-		/* prepare information for widgets */
-		if ($prepare)
+		foreach ($widgets as &$widget)
 		{
-			foreach ($widgets as &$widget)
+			$this->_prepareWidgetMandatory($widget);
+
+			if ($prepare)
 			{
 				$this->prepareWidget($widget);
 			}
@@ -109,9 +110,11 @@ class WidgetFramework_Model_Widget extends XenForo_Model
 				ORDER BY display_order ASC
 				", 'widget_id', array($widgetPageId));
 
-		if ($prepare)
+		foreach ($widgets as &$widget)
 		{
-			foreach ($widgets as &$widget)
+			$this->_prepareWidgetMandatory($widget);
+
+			if ($prepare)
 			{
 				$this->prepareWidget($widget);
 			}
@@ -154,6 +157,8 @@ class WidgetFramework_Model_Widget extends XenForo_Model
 				WHERE widget_id = ?
 				", array($widgetId));
 
+		$this->_prepareWidgetMandatory($widget);
+
 		return $widget;
 	}
 
@@ -168,18 +173,6 @@ class WidgetFramework_Model_Widget extends XenForo_Model
 		if (empty($widget))
 		{
 			return $widget;
-		}
-
-		$widget['options'] = @unserialize($widget['options']);
-		if (empty($widget['options']))
-		{
-			$widget['options'] = array();
-		}
-
-		$widget['template_for_hooks'] = @unserialize($widget['template_for_hooks']);
-		if (empty($widget['template_for_hooks']))
-		{
-			$widget['template_for_hooks'] = array();
 		}
 
 		$renderer = WidgetFramework_Core::getRenderer($widget['class'], false);
@@ -206,6 +199,24 @@ class WidgetFramework_Model_Widget extends XenForo_Model
 		}
 
 		return $widget;
+	}
+
+	protected function _prepareWidgetMandatory(array &$widget)
+	{
+		if (!is_array($widget['options']))
+		{
+			$widget['options'] = @unserialize($widget['options']);
+		}
+		if (empty($widget['options']))
+		{
+			$widget['options'] = array();
+		}
+
+		$widget['template_for_hooks'] = @unserialize($widget['template_for_hooks']);
+		if (empty($widget['template_for_hooks']))
+		{
+			$widget['template_for_hooks'] = array();
+		}
 	}
 
 }

commit a2eecf716df42e013d3f21c5719a875f9d0341bf

fixed issue with template_for_hooks in global widgets cache caused by e0cc869a90409a4e2a86755b113f228965ac467d

library/WidgetFramework/Core.php
@@ -74,7 +74,9 @@ class WidgetFramework_Core
 	public function bootstrap()
 	{
 		if (defined('WIDGET_FRAMEWORK_LOADED'))
+		{
 			return false;
+		}
 
 		$globalWidgets = $this->_getModelWidget()->getGlobalWidgets(true, false);
 		$this->_getModelWidget()->reverseNegativeDisplayOrderWidgets($globalWidgets);
@@ -112,7 +114,9 @@ class WidgetFramework_Core
 		foreach ($widgets as &$widget)
 		{
 			if (empty($widget['active']))
+			{
 				continue;
+			}
 
 			$widgetPositions = explode(',', $widget['position']);
 

library/WidgetFramework/Model/Widget.php
@@ -212,7 +212,10 @@ class WidgetFramework_Model_Widget extends XenForo_Model
 			$widget['options'] = array();
 		}
 
-		$widget['template_for_hooks'] = @unserialize($widget['template_for_hooks']);
+		if (!is_array($widget['template_for_hooks']))
+		{
+			$widget['template_for_hooks'] = @unserialize($widget['template_for_hooks']);
+		}
 		if (empty($widget['template_for_hooks']))
 		{
 			$widget['template_for_hooks'] = array();