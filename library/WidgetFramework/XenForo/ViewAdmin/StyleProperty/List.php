ae0f3d850d31f251540461ee509e4183bf1397a1
Dao Hoang Son <daohoangson@gmail.com>
2014-08-07 14:20:45 +0700

    fixed bug style properties out of order

--- library/WidgetFramework/XenForo/ViewAdmin/StyleProperty/List.php
+++ library/WidgetFramework/XenForo/ViewAdmin/StyleProperty/List.php
@@ -6,7 +6,10 @@ class WidgetFramework_XenForo_ViewAdmin_StyleProperty_List extends XFCP_WidgetFr
 	{
 		parent::renderHtml();
 
-		ksort($this->_params['scalars']);
+		if (!empty($this->_params['group']['group_name']) AND $this->_params['group']['group_name'] === 'widget_framework')
+		{
+			ksort($this->_params['scalars']);
+		}
 	}
 
 }
