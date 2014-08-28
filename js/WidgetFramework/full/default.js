bc204d2733690687305c950a2d262b9ec0fc0524
Dao Hoang Son <daohoangson@gmail.com>
2014-08-29 00:52:57 +0700

    improved ProfilePosts renderer to mimic XenForo 1.4.0 RC1

---
+++ js/WidgetFramework/full/default.js
@@ -0,0 +1,8 @@
+! function($, window, document, _undefined)
+{
+	// TODO
+
+	// *********************************************************************
+
+	XenForo.register('.WidgetFramework_WidgetRenderer_ProfilePosts form.statusPoster', 'XenForo.ProfilePoster');
+}(jQuery, this, document); 
