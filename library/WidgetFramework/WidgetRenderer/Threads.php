xfrocks/bdWidgetFramework

commit 87cd37e0b73e6471f72aa8aefc560f4d16430b00

updated Threads renderer for XenForo 1.5+

library/WidgetFramework/WidgetRenderer/Threads.php
@@ -21,6 +21,10 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
             switch ($widget['options']['type']) {
                 case 'recent':
                 case 'recent_first_poster':
+                    if (XenForo_Application::$versionId > 1050000) {
+                        return new XenForo_Phrase('new_posts');
+                    }
+
                     return new XenForo_Phrase('wf_widget_threads_type_recent');
                 case 'latest_replies':
                     return new XenForo_Phrase('wf_widget_threads_type_latest_replies');
@@ -73,14 +77,17 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
     {
         $params = $template->getParams();
 
-        $forums = $this->_helperPrepareForumsOptionSource(empty($params['options']['forums']) ? array() : $params['options']['forums'], true);
+        $forums = empty($params['options']['forums']) ? array() : $params['options']['forums'];
+        $forums = $this->_helperPrepareForumsOptionSource($forums, true);
 
         /** @var XenForo_Model_ThreadPrefix $threadPrefixModel */
         $threadPrefixModel = WidgetFramework_Core::getInstance()->getModelFromCache('XenForo_Model_ThreadPrefix');
         $prefixes = $threadPrefixModel->getPrefixOptions();
         foreach ($prefixes as $prefixGroupId => &$groupPrefixes) {
             foreach ($groupPrefixes as &$prefix) {
-                if (!empty($params['options']['prefixes']) AND in_array($prefix['value'], $params['options']['prefixes'])) {
+                if (!empty($params['options']['prefixes'])
+                    && in_array($prefix['value'], $params['options']['prefixes'])
+                ) {
                     $prefix['selected'] = true;
                 }
             }
@@ -116,7 +123,11 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
         return 'wf_widget_threads';
     }
 
-    protected function _render(array $widget, $positionCode, array $params, XenForo_Template_Abstract $renderTemplateObject)
+    protected function _render(
+        array $widget,
+        $positionCode,
+        array $params,
+        XenForo_Template_Abstract $renderTemplateObject)
     {
         if (empty($widget['options']['limit'])) {
             $widget['options']['limit'] = 5;
@@ -163,6 +174,18 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
         return $renderTemplateObject->render();
     }
 
+    protected function _getExtraDataLink(array $widget)
+    {
+        if (XenForo_Application::$versionId > 1050000
+            && $widget['options']['type'] === 'recent'
+        ) {
+            return XenForo_Link::buildPublicLink('find-new/posts');
+        }
+
+        return parent::_getExtraDataLink($widget);
+    }
+
+
     public function useUserCache(array $widget)
     {
         if (!empty($widget['options']['as_guest'])) {
@@ -176,7 +199,9 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
 
     public function useWrapper(array $widget)
     {
-        if (!empty($widget['options']['layout']) AND $widget['options']['layout'] === 'full') {
+        if (!empty($widget['options']['layout'])
+            && $widget['options']['layout'] === 'full'
+        ) {
             // using full layout, do not use wrapper
             return false;
         }
@@ -190,9 +215,12 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
             if (!empty($widget['_ajaxLoadParams']['forumIds'])) {
                 $suffix[] = 'f' . implode('', $widget['_ajaxLoadParams']['forumIds']);
             }
-        } elseif ($this->_helperDetectSpecialForums($widget['options']['forums'])) {
+        } elseif ($this->_helperDetectSpecialForums(empty($params['options']['forums'])
+            ? array() : $params['options']['forums'])
+        ) {
             if (isset($params['forum'])) {
-                $forumIds = $this->_helperGetForumIdsFromOption($widget['options']['forums'], $params, empty($widget['options']['as_guest']) ? false : true);
+                $forumIds = $this->_helperGetForumIdsFromOption($widget['options']['forums'], $params,
+                    empty($widget['options']['as_guest']) ? false : true);
                 $suffix[] = 'f' . implode('', $forumIds);
             }
         }
@@ -205,10 +233,22 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
         $core = WidgetFramework_Core::getInstance();
         $layoutNeedPost = $renderTemplateObject->getParam('layoutNeedPost');
 
+        if ($positionCode === 'forum_list'
+            && XenForo_Application::$versionId > 1050000
+            && isset($params['threads'])
+            && !$layoutNeedPost
+            && $widget['options']['type'] === 'recent'
+            && $widget['options']['limit'] == XenForo_Application::getOptions()->get('forumListNewPosts')
+        ) {
+            return $params['threads'];
+        }
+
         /* @var $threadModel XenForo_Model_Thread */
         $threadModel = $core->getModelFromCache('XenForo_Model_Thread');
 
-        $forumIds = $this->_helperGetForumIdsFromOption($widget['options']['forums'], $params, empty($widget['options']['as_guest']) ? false : true);
+        $forumIds = $this->_helperGetForumIdsFromOption(empty($params['options']['forums'])
+            ? array() : $params['options']['forums'], $params,
+            empty($widget['options']['as_guest']) ? false : true);
         if (!empty($widget['_ajaxLoadParams']['forumIds'])) {
             $forumIds = $widget['_ajaxLoadParams']['forumIds'];
         }
@@ -235,7 +275,9 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
 
         // process sticky
         // since 2.4.7
-        if (isset($widget['options']['sticky']) AND is_numeric($widget['options']['sticky'])) {
+        if (isset($widget['options']['sticky'])
+            && is_numeric($widget['options']['sticky'])
+        ) {
             $conditions['sticky'] = intval($widget['options']['sticky']);
         }
 
@@ -290,19 +332,23 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
                 )));
                 break;
             case 'popular':
-                $threads = $threadModel->getThreads(array_merge($conditions, array(WidgetFramework_XenForo_Model_Thread::CONDITIONS_POST_DATE => array(
-                    '>',
-                    XenForo_Application::$time - $widget['options']['cutoff'] * 86400
-                ))), array_merge($fetchOptions, array(
+                $threads = $threadModel->getThreads(array_merge($conditions, array(
+                    WidgetFramework_XenForo_Model_Thread::CONDITIONS_POST_DATE => array(
+                        '>',
+                        XenForo_Application::$time - $widget['options']['cutoff'] * 86400
+                    )
+                )), array_merge($fetchOptions, array(
                     'order' => 'view_count',
                     'orderDirection' => 'desc',
                 )));
                 break;
             case 'most_replied':
-                $threads = $threadModel->getThreads(array_merge($conditions, array(WidgetFramework_XenForo_Model_Thread::CONDITIONS_POST_DATE => array(
-                    '>',
-                    XenForo_Application::$time - $widget['options']['cutoff'] * 86400
-                ))), array_merge($fetchOptions, array(
+                $threads = $threadModel->getThreads(array_merge($conditions, array(
+                    WidgetFramework_XenForo_Model_Thread::CONDITIONS_POST_DATE => array(
+                        '>',
+                        XenForo_Application::$time - $widget['options']['cutoff'] * 86400
+                    )
+                )), array_merge($fetchOptions, array(
                     'order' => 'reply_count',
                     'orderDirection' => 'desc',
                 )));
@@ -315,10 +361,12 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
                 }
                 break;
             case 'most_liked':
-                $threads = $threadModel->getThreads(array_merge($conditions, array(WidgetFramework_XenForo_Model_Thread::CONDITIONS_POST_DATE => array(
-                    '>',
-                    XenForo_Application::$time - $widget['options']['cutoff'] * 86400
-                ))), array_merge($fetchOptions, array(
+                $threads = $threadModel->getThreads(array_merge($conditions, array(
+                    WidgetFramework_XenForo_Model_Thread::CONDITIONS_POST_DATE => array(
+                        '>',
+                        XenForo_Application::$time - $widget['options']['cutoff'] * 86400
+                    )
+                )), array_merge($fetchOptions, array(
                     'order' => 'first_post_likes',
                     'orderDirection' => 'desc',
                 )));
@@ -331,7 +379,9 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
                 }
                 break;
             case 'polls':
-                $threads = $threadModel->getThreads(array_merge($conditions, array(WidgetFramework_XenForo_Model_Thread::CONDITIONS_DISCUSSION_TYPE => 'poll')), array_merge($fetchOptions, array(
+                $threads = $threadModel->getThreads(array_merge($conditions, array(
+                    WidgetFramework_XenForo_Model_Thread::CONDITIONS_DISCUSSION_TYPE => 'poll'
+                )), array_merge($fetchOptions, array(
                     'order' => 'post_date',
                     'orderDirection' => 'desc',
                 )));
@@ -379,10 +429,13 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
         /** @var XenForo_Model_Post $postModel */
         $postModel = $core->getModelFromCache('XenForo_Model_Post');
 
-        $nodePermissions = $nodeModel->getNodePermissionsForPermissionCombination(empty($widget['options']['as_guest']) ? null : 1);
+        $permissionCombinationId = empty($widget['options']['as_guest']) ? null : 1;
+        $nodePermissions = $nodeModel->getNodePermissionsForPermissionCombination($permissionCombinationId);
 
         $viewObj = self::getViewObject($params, $renderTemplateObject);
-        if ($layoutNeedPost AND !empty($viewObj)) {
+        if ($layoutNeedPost
+            && !empty($viewObj)
+        ) {
             $bbCodeFormatter = XenForo_BbCode_Formatter_Base::create('Base', array('view' => $viewObj));
             if (XenForo_Application::$versionId < 1020000) {
                 // XenForo 1.1.x
@@ -464,18 +517,26 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
                 continue;
             }
 
-            if (!$threadModel->canViewThread($threadRef, $threadForumRef, $null, $threadPermissionsRef, $viewingUser)) {
+            if (!$threadModel->canViewThreadAndContainer($threadRef, $threadForumRef, $null,
+                $threadPermissionsRef, $viewingUser)
+            ) {
                 unset($threads[$threadId]);
                 continue;
             }
 
-            if (!empty($bbCodeParser) AND !empty($bbCodeOptions)) {
+            if (!empty($bbCodeParser)
+                && !empty($bbCodeOptions)
+            ) {
                 $threadBbCodeOptions = $bbCodeOptions;
-                $threadBbCodeOptions['states']['viewAttachments'] = $threadModel->canViewAttachmentsInThread($threadRef, $threadForumRef, $null, $threadPermissionsRef, $viewingUser);
-                $threadRef['messageHtml'] = XenForo_ViewPublic_Helper_Message::getBbCodeWrapper($threadRef, $bbCodeParser, $threadBbCodeOptions);
+                $threadBbCodeOptions['states']['viewAttachments'] =
+                    $threadModel->canViewAttachmentsInThread($threadRef, $threadForumRef, $null,
+                        $threadPermissionsRef, $viewingUser);
+                $threadRef['messageHtml'] =
+                    XenForo_ViewPublic_Helper_Message::getBbCodeWrapper($threadRef, $bbCodeParser, $threadBbCodeOptions);
             }
 
-            $threadRef = $threadModel->WidgetFramework_prepareThreadForRendererThreads($threadRef, $threadForumRef, $threadPermissionsRef, $viewingUser);
+            $threadRef = $threadModel->WidgetFramework_prepareThreadForRendererThreads($threadRef, $threadForumRef,
+                $threadPermissionsRef, $viewingUser);
         }
     }
 
@@ -484,7 +545,8 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
         $ajaxLoadParams = parent::_getAjaxLoadParams($widget, $positionCode, $params, $template);
 
         if ($this->_helperDetectSpecialForums($widget['options']['forums'])) {
-            $forumIds = $this->_helperGetForumIdsFromOption($widget['options']['forums'], $params, empty($widget['options']['as_guest']) ? false : true);
+            $forumIds = $this->_helperGetForumIdsFromOption($widget['options']['forums'], $params,
+                empty($widget['options']['as_guest']) ? false : true);
             $ajaxLoadParams['forumIds'] = $forumIds;
         }
 

library/WidgetFramework/Installer.php
@@ -126,6 +126,15 @@ class WidgetFramework_Installer
 				");
             }
 
+            if (XenForo_Application::$versionId > 1050000) {
+                $db->query("
+					INSERT INTO `xf_widget`
+						(title, class, options, position, display_order)
+					VALUES
+						('', 'WidgetFramework_WidgetRenderer_Threads', 'a:2:{s:4:\"type\";s:6:\"recent\";s:5:\"limit\";i:5;}', 'forum_list', 20)
+				");
+            }
+
             if (XenForo_Application::$versionId < 1020000) {
                 $db->query("
 					INSERT INTO `xf_widget`

library/WidgetFramework/XenForo/Model/Thread.php
@@ -15,7 +15,9 @@ class WidgetFramework_XenForo_Model_Thread extends XFCP_WidgetFramework_XenForo_
         $result = parent::prepareThreadConditions($conditions, $fetchOptions);
         $sqlConditions = array($result);
 
-        if (!empty($conditions[self::CONDITIONS_POST_DATE]) AND is_array($conditions[self::CONDITIONS_POST_DATE])) {
+        if (!empty($conditions[self::CONDITIONS_POST_DATE])
+            && is_array($conditions[self::CONDITIONS_POST_DATE])
+        ) {
             list($operator, $cutOff) = $conditions[self::CONDITIONS_POST_DATE];
             $this->assertValidCutOffOperator($operator);
             $sqlConditions[] = "thread.post_date $operator " . $this->_getDb()->quote($cutOff);
@@ -23,9 +25,11 @@ class WidgetFramework_XenForo_Model_Thread extends XFCP_WidgetFramework_XenForo_
 
         if (isset($conditions[self::CONDITIONS_DISCUSSION_TYPE])) {
             if (is_array($conditions[self::CONDITIONS_DISCUSSION_TYPE])) {
-                $sqlConditions[] = "thread.discussion_type IN (" . $this->_getDb()->quote($conditions[self::CONDITIONS_DISCUSSION_TYPE]) . ")";
+                $sqlConditions[] = "thread.discussion_type IN ("
+                    . $this->_getDb()->quote($conditions[self::CONDITIONS_DISCUSSION_TYPE]) . ")";
             } else {
-                $sqlConditions[] = "thread.discussion_type = " . $this->_getDb()->quote($conditions[self::CONDITIONS_DISCUSSION_TYPE]);
+                $sqlConditions[] = "thread.discussion_type = "
+                    . $this->_getDb()->quote($conditions[self::CONDITIONS_DISCUSSION_TYPE]);
             }
         }
 
@@ -123,7 +127,11 @@ class WidgetFramework_XenForo_Model_Thread extends XFCP_WidgetFramework_XenForo_
         return compact('selectFields', 'joinTables', 'orderClause');
     }
 
-    public function WidgetFramework_prepareThreadForRendererThreads(array $thread, array $forum, array $nodePermissions = null, array $viewingUser = null)
+    public function WidgetFramework_prepareThreadForRendererThreads(
+        array $thread,
+        array $forum,
+        array $nodePermissions = null,
+        array $viewingUser = null)
     {
         $thread = $this->prepareThread($thread, $forum, $nodePermissions, $viewingUser);
 

library/WidgetFramework/addon-widget_framework.xml
@@ -1007,8 +1007,8 @@ array $widget, $positionCode, array $params, XenForo_Template_Abstract $renderTe
     <phrase title="wf_widget_threads_type_new" global_cache="1" version_id="1" version_string="0.9"><![CDATA[New Threads]]></phrase>
     <phrase title="wf_widget_threads_type_polls" global_cache="1" version_id="111" version_string="2.6.0-dev"><![CDATA[Polls]]></phrase>
     <phrase title="wf_widget_threads_type_popular" global_cache="1" version_id="65" version_string="2.3.6"><![CDATA[Most Viewed Theads]]></phrase>
-    <phrase title="wf_widget_threads_type_recent" global_cache="1" version_id="96" version_string="2.5.0d"><![CDATA[Recent Threads]]></phrase>
-    <phrase title="wf_widget_threads_type_recent_first_poster" version_id="96" version_string="2.5.0d"><![CDATA[Recent Threads (use first poster avatar)]]></phrase>
+    <phrase title="wf_widget_threads_type_recent" global_cache="1" version_id="112" version_string="2.6.0-dev"><![CDATA[New Posts]]></phrase>
+    <phrase title="wf_widget_threads_type_recent_first_poster" version_id="112" version_string="2.6.0-dev"><![CDATA[New Posts (use first poster avatar)]]></phrase>
     <phrase title="wf_widget_users_display_mode_avatar_only" version_id="24" version_string="1.2.2.2"><![CDATA[Avatar Only]]></phrase>
     <phrase title="wf_widget_users_display_mode_avatar_only_bigger" version_id="32" version_string="1.4.2"><![CDATA[Avatar Only (Bigger)]]></phrase>
     <phrase title="wf_widget_users_display_mode_list" version_id="24" version_string="1.2.2.2"><![CDATA[List]]></phrase>
@@ -2022,116 +2022,120 @@ All customization should go to wf_default_EXTRA.css
 		<dd><xen:username user="$boardTotals.latestUser" /></dd></dl>
 	<!-- slot: forum_stats_extra -->
 </div>]]></template>
-    <template title="wf_widget_threads" version_id="102" version_string="2.5.0d"><![CDATA[<xen:if is="{$layout} == 'sidebar'">
+    <template title="wf_widget_threads" version_id="112" version_string="2.6.0-dev"><![CDATA[<xen:if is="{$layout} == 'sidebar'">
 
-	<xen:if hascontent="true">
-		<div class="avatarList">
-			<ul>
-				<xen:contentcheck>
-					<xen:if is="{$widget.options.type} == 'recent' OR {$widget.options.type} == 'latest_replies'">
+<xen:if is="{xen:helper WidgetFramework_getOption, applicationVersionId} > 1050000
+	AND {$widget.options.type} == 'recent'">
+<xen:include template="sidebar_new_posts" />
+<xen:else />
+	
+<xen:if hascontent="true">
+<div class="avatarList">
+	<ul>
+		<xen:contentcheck>
+		<xen:if is="{$widget.options.type} == 'recent' OR {$widget.options.type} == 'latest_replies'">
 
-						<xen:foreach loop="$threads" value="$thread">
-							<xen:set var="$user"><xen:username user="$thread" rich="{xen:property wf_threads_rich}" /></xen:set>
-							<xen:include template="wf_widget_threads_thread_sidebar">
-								<xen:set var="$_threadLink">{xen:if '{$visitor.user_id} > 0', {xen:link threads/unread, $thread}, {xen:link posts, {xen:array 'post_id={$thread.last_post_id}'}}}</xen:set>
-								<xen:set var="$_secondRow">
-									{xen:phrase wf_x_replied, 'user={xen:raw $user}'} <xen:datetime time="{$thread.last_post_date}" />
-								</xen:set>
-							</xen:include>
-						</xen:foreach>
+		<xen:foreach loop="$threads" value="$thread">
+		<xen:set var="$user"><xen:username user="$thread" rich="{xen:property wf_threads_rich}" /></xen:set>
+		<xen:include template="wf_widget_threads_thread_sidebar">
+			<xen:set var="$_threadLink">{xen:if '{$visitor.user_id} > 0', {xen:link threads/unread, $thread}, {xen:link posts, {xen:array 'post_id={$thread.last_post_id}'}}}</xen:set>
+			<xen:set var="$_secondRow">
+				{xen:phrase wf_x_replied, 'user={xen:raw $user}'} <xen:datetime time="{$thread.last_post_date}" />
+			</xen:set>
+		</xen:include>
+		</xen:foreach>
 
-					<xen:elseif is="{$widget.options.type} == 'most_replied'" />
+		<xen:elseif is="{$widget.options.type} == 'most_replied'" />
 
-						<xen:foreach loop="$threads" value="$thread">
-							<xen:set var="$user"><xen:username user="$thread" rich="{xen:property wf_threads_rich}" /></xen:set>
-							<xen:include template="wf_widget_threads_thread_sidebar">
-								<xen:set var="$_secondRow">
-									{xen:phrase wf_x_posted, 'user={xen:raw $user}'}, {xen:phrase replies}: {xen:number $thread.reply_count}
-								</xen:set>
-							</xen:include>
-						</xen:foreach>
+		<xen:foreach loop="$threads" value="$thread">
+		<xen:set var="$user"><xen:username user="$thread" rich="{xen:property wf_threads_rich}" /></xen:set>
+		<xen:include template="wf_widget_threads_thread_sidebar">
+			<xen:set var="$_secondRow">
+				{xen:phrase wf_x_posted, 'user={xen:raw $user}'}, {xen:phrase replies}: {xen:number $thread.reply_count}
+			</xen:set>
+		</xen:include>
+		</xen:foreach>
 
-					<xen:elseif is="{$widget.options.type} == 'most_liked'" />
+		<xen:elseif is="{$widget.options.type} == 'most_liked'" />
 
-						<xen:foreach loop="$threads" value="$thread">
-							<xen:set var="$user"><xen:username user="$thread" rich="{xen:property wf_threads_rich}" /></xen:set>
-							<xen:include template="wf_widget_threads_thread_sidebar">
-								<xen:set var="$_secondRow">
-									{xen:phrase wf_x_posted, 'user={xen:raw $user}'},
-									{xen:phrase likes}: <a href="{xen:link 'posts/likes', {xen:array 'post_id={$thread.first_post_id}'}}" class="OverlayTrigger">{xen:number $thread.first_post_likes}</a>
-								</xen:set>
-							</xen:include>
-						</xen:foreach>
+		<xen:foreach loop="$threads" value="$thread">
+		<xen:set var="$user"><xen:username user="$thread" rich="{xen:property wf_threads_rich}" /></xen:set>
+		<xen:include template="wf_widget_threads_thread_sidebar">
+			<xen:set var="$_secondRow">
+				{xen:phrase wf_x_posted, 'user={xen:raw $user}'},
+				{xen:phrase likes}: <a href="{xen:link 'posts/likes', {xen:array 'post_id={$thread.first_post_id}'}}" class="OverlayTrigger">{xen:number $thread.first_post_likes}</a>
+			</xen:set>
+		</xen:include>
+		</xen:foreach>
 
-					<xen:else />
+		<xen:else />
 
-						<xen:foreach loop="$threads" value="$thread">
-							<xen:set var="$user"><xen:username user="$thread" rich="{xen:property wf_threads_rich}" /></xen:set>
-							<xen:include template="wf_widget_threads_thread_sidebar">
-								<xen:set var="$_secondRow">
-									{xen:phrase wf_x_posted, 'user={xen:raw $user}'} <xen:datetime time="{$thread.post_date}" />
-								</xen:set>
-							</xen:include>
-						</xen:foreach>
+		<xen:foreach loop="$threads" value="$thread">
+		<xen:set var="$user"><xen:username user="$thread" rich="{xen:property wf_threads_rich}" /></xen:set>
+		<xen:include template="wf_widget_threads_thread_sidebar">
+			<xen:set var="$_secondRow">
+				{xen:phrase wf_x_posted, 'user={xen:raw $user}'} <xen:datetime time="{$thread.post_date}" />
+			</xen:set>
+		</xen:include>
+		</xen:foreach>
 
-					</xen:if>
-				</xen:contentcheck>
-			</ul>
-		</div>
+		</xen:if>
+		</xen:contentcheck>
+	</ul>
+</div>
 
-		<xen:include template="preview_tooltip" />
-	</xen:if>
+<xen:include template="preview_tooltip" />
+</xen:if>
+</xen:if>
 
 <xen:elseif is="{$layout} == 'list'" />
 	
-	<xen:if hascontent="true">
-		<div class="discussionList">
-			<div class="DiscussionList">
+<xen:if hascontent="true">
+<div class="discussionList">
+	<div class="DiscussionList">
 
-				<dl class="sectionHeaders">
-					<dt class="posterAvatar"><a><span>&nbsp;</span></a></dt>
-					<dd class="main">
-						<a class="title"><span>{xen:phrase title}</span></a>
-						<a class="postDate"><span>{xen:phrase start_date}</span></a>
-					</dd>
-					<dd class="stats">
-						<a class="major"><span>{xen:phrase replies}</span></a>
-						<a class="minor"><span>{xen:phrase views}</span></a>
-					</dd>
-					<dd class="lastPost"><a><span>{xen:phrase last_message}</span></a></dd>
-				</dl>
+		<dl class="sectionHeaders">
+			<dt class="posterAvatar"><a><span>&nbsp;</span></a></dt>
+			<dd class="main">
+				<a class="title"><span>{xen:phrase title}</span></a>
+				<a class="postDate"><span>{xen:phrase start_date}</span></a>
+			</dd>
+			<dd class="stats">
+				<a class="major"><span>{xen:phrase replies}</span></a>
+				<a class="minor"><span>{xen:phrase views}</span></a>
+			</dd>
+			<dd class="lastPost"><a><span>{xen:phrase last_message}</span></a></dd>
+		</dl>
 
-				<ol class="discussionListItems">
-					<xen:contentcheck>
-						<xen:foreach loop="$threads" value="$thread">
-							<xen:include template="thread_list_item" />
-						</xen:foreach>
-					</xen:contentcheck>
-				</ol>
+		<ol class="discussionListItems">
+			<xen:contentcheck>
+			<xen:foreach loop="$threads" value="$thread">
+			<xen:include template="thread_list_item" />
+			</xen:foreach>
+			</xen:contentcheck>
+		</ol>
 
-			</div>
-		</div>
+	</div>
+</div>
 
-		<xen:include template="preview_tooltip" />
-	</xen:if>
+<xen:include template="preview_tooltip" />
+</xen:if>
 
 <xen:elseif is="{$layout} == 'full'" />
 
-	<xen:if hascontent="true">
-		<div class="WidgetFramework_WidgetRenderer_Threads_FullThreadList">
-			<xen:contentcheck>
+<xen:if hascontent="true">
+<div class="WidgetFramework_WidgetRenderer_Threads_FullThreadList">
+	<xen:contentcheck>
+	<xen:foreach loop="$threads" value="$thread">
+	<xen:include template="wf_widget_threads_thread_full" />
+	<xen:edithint template="wf_widget_threads_thread_full_info" />
+	</xen:foreach>
+	</xen:contentcheck>
+</div>
 
-				<xen:foreach loop="$threads" value="$thread">
-					<xen:include template="wf_widget_threads_thread_full" />
-					<xen:edithint template="wf_widget_threads_thread_full_info" />
-				</xen:foreach>
-
-			</xen:contentcheck>
-		</div>
-		
-		<xen:edithint template="message" />
-		<xen:edithint template="wf_default.css" />
-	</xen:if>
+<xen:edithint template="message" />
+<xen:edithint template="wf_default.css" />
+</xen:if>
 
 </xen:if>]]></template>
     <template title="wf_widget_threads_thread_full" version_id="96" version_string="2.5.0d"><![CDATA[<xen:require css="wf_default.css" />

commit 2cb4efaf5b02a7356ea4d8085a40f20238ca1b61

fixed bug forums option doesn't work in Threads renderer (caused by 87cd37e)

library/WidgetFramework/WidgetRenderer/Threads.php
@@ -253,8 +253,8 @@ class WidgetFramework_WidgetRenderer_Threads extends WidgetFramework_WidgetRende
         /* @var $threadModel XenForo_Model_Thread */
         $threadModel = $core->getModelFromCache('XenForo_Model_Thread');
 
-        $forumIds = $this->_helperGetForumIdsFromOption(empty($params['options']['forums'])
-            ? array() : $params['options']['forums'], $params,
+        $forumIds = $this->_helperGetForumIdsFromOption(empty($widget['options']['forums'])
+            ? array() : $widget['options']['forums'], $params,
             empty($widget['options']['as_guest']) ? false : true);
         if (!empty($widget['_ajaxLoadParams']['forumIds'])) {
             $forumIds = $widget['_ajaxLoadParams']['forumIds'];