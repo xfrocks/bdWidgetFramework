0160b7ed12430a3e1a6811a77d75212d364a0768
Dao Hoang Son <daohoangson@gmail.com>
2016-06-09 10:55:08 +0700

    improved renderer UserFollow

--- library/WidgetFramework/WidgetRenderer/UserFollow.php
+++ library/WidgetFramework/WidgetRenderer/UserFollow.php
@@ -2,17 +2,56 @@
 
 class WidgetFramework_WidgetRenderer_UserFollow extends WidgetFramework_WidgetRenderer
 {
+    public static /** @noinspection HtmlUnknownTarget */
+        $countFormat = '<a href="%2$s" class="count OverlayTrigger">%1$d</a>';
+
+    protected static $_links = array();
+    protected static $_counts = array();
+
+    public function extraPrepareTitle(array $widget)
+    {
+        if (empty($widget['title'])) {
+            if (!empty($widget['options']['type'])
+                && $widget['options']['type'] === 'followers'
+            ) {
+                $phrase = new XenForo_Phrase('followers');
+                $countKey = 'followersCount';
+            } else {
+                $phrase = new XenForo_Phrase('following');
+                $countKey = 'followingCount';
+            }
+
+            if (isset(self::$_counts[$widget['widget_id']][$countKey])
+                && !empty(self::$_links[$widget['widget_id']]
+                && empty($widget['group_id']))
+            ) {
+                return WidgetFramework_Helper_String::createArrayOfStrings(array(
+                    $phrase,
+                    sprintf(self::$countFormat,
+                        self::$_counts[$widget['widget_id']][$countKey],
+                        self::$_links[$widget['widget_id']])
+                ));
+            } else {
+                return $phrase;
+            }
+        }
+
+        return parent::extraPrepareTitle($widget);
+    }
+
     protected function _getConfiguration()
     {
         return array(
             'name' => 'User: Follow',
-            'useWrapper' => false,
+            'options' => array(
+                'type' => XenForo_Input::STRING,
+            )
         );
     }
 
     protected function _getOptionsTemplate()
     {
-        return false;
+        return 'wf_widget_options_user_follow';
     }
 
     protected function _getRenderTemplate(array $widget, $positionCode, array $params)
@@ -29,32 +68,54 @@ class WidgetFramework_WidgetRenderer_UserFollow extends WidgetFramework_WidgetRe
         /** @var XenForo_Model_User $userModel */
         $userModel = WidgetFramework_Core::getInstance()->getModelFromCache('XenForo_Model_User');
 
-        $user = XenForo_Visitor::getInstance();
-        $userId = $user['user_id'];
+        $user = XenForo_Visitor::getInstance()->toArray();
 
-        if (!empty($user['following'])) {
-            $followingToShowCount = 6;
-            $followingCount = substr_count($user['following'], ',') + 1;
-
-            $following = $userModel->getFollowedUserProfiles($userId, $followingToShowCount, 'RAND()');
-            if (count($following) < $followingToShowCount) {
-                $followingCount = count($following);
+        $limit = !empty($widget['options']['limit']) ? $widget['options']['limit'] : 6;
+        $type = 'following';
+        if (!empty($widget['options']['type'])) {
+            switch ($widget['options']['type']) {
+                case 'following':
+                case 'followers':
+                    $type = $widget['options']['type'];
+                    break;
             }
-        } else {
-            $followingCount = 0;
-            $following = array();
         }
 
-        $followersCount = $userModel->countUsersFollowingUserId($userId);
-        $followers = $userModel->getUsersFollowingUserId($userId, 6, 'RAND()');
+        switch ($type) {
+            case 'following':
+                if (!empty($user['following'])) {
+                    $followingCount = substr_count($user['following'], ',') + 1;
 
-        $renderTemplateObject->setParams(array(
-            'followingCount' => $followingCount,
-            'followersCount' => $followersCount,
+                    $following = $userModel->getFollowedUserProfiles($user['user_id'], $limit, 'RAND()');
+                    if (count($following) < $limit) {
+                        $followingCount = count($following);
+                    }
+                } else {
+                    $followingCount = 0;
+                    $following = array();
+                }
 
-            'following' => $following,
-            'followers' => $followers,
-        ));
+                $renderTemplateObject->setParams(array(
+                    'followingCount' => $followingCount,
+                    'following' => $following,
+                ));
+                self::$_links[$widget['widget_id']] = XenForo_Link::buildPublicLink('members/following', $user);
+                self::$_counts[$widget['widget_id']]['followingCount'] = $followingCount;
+                break;
+            case 'followers':
+                $followersCount = $userModel->countUsersFollowingUserId($user['user_id']);
+                $followers = $userModel->getUsersFollowingUserId($user['user_id'], 6, 'RAND()');
+
+                $renderTemplateObject->setParams(array(
+                    'followersCount' => $followersCount,
+                    'followers' => $followers,
+                ));
+                self::$_links[$widget['widget_id']] = XenForo_Link::buildPublicLink('members/followers', $user);
+                self::$_counts[$widget['widget_id']]['followersCount'] = $followersCount;
+                break;
+        }
+
+        $renderTemplateObject->setParam('user', $user);
 
         return $renderTemplateObject->render();
     }
