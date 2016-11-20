xfrocks/bdWidgetFramework

commit 9c6e0cfe58d4c8f331d320d8b6c015edc0927550

Simplify WidgetFramework_WidgetRenderer::getViewObject

library/WidgetFramework/Listener.php
@@ -5,17 +5,7 @@ class WidgetFramework_Listener
     const UPDATER_URL = 'https://xfrocks.com/api/index.php?updater';
 
     /**
-     * @var XenForo_Dependencies_Abstract
-     */
-    public static $dependencies = null;
-
-    /**
-     * @var XenForo_FrontController
-     */
-    public static $fc = null;
-
-    /**
-     * @var XenForo_ViewRenderer_Abstract
+     * @var XenForo_ViewRenderer_HtmlPublic
      */
     public static $viewRenderer = null;
 
@@ -23,13 +13,8 @@ class WidgetFramework_Listener
     protected static $_saveLayoutEditorRendered = false;
     protected static $_layoutEditorRendered = array();
 
-    public static function init_dependencies(
-        XenForo_Dependencies_Abstract $dependencies,
-        /** @noinspection PhpUnusedParameterInspection */
-        array $data
-    ) {
-        self::$dependencies = $dependencies;
-
+    public static function init_dependencies(XenForo_Dependencies_Abstract $dependencies, array $data)
+    {
         XenForo_Template_Helper_Core::$helperCallbacks[strtolower('WidgetFramework_snippet')] = array(
             'WidgetFramework_Template_Helper_Core',
             'snippet'
@@ -244,10 +229,10 @@ class WidgetFramework_Listener
         /** @noinspection PhpUnusedParameterInspection */
         array &$containerParams
     ) {
-        self::$fc = $fc;
-        self::$viewRenderer = $viewRenderer;
-
-        if ($fc->getDependencies() instanceof XenForo_Dependencies_Public) {
+        if (self::$viewRenderer === null
+            && $viewRenderer instanceof XenForo_ViewRenderer_HtmlPublic
+        ) {
+            self::$viewRenderer = $viewRenderer;
             WidgetFramework_Core::getInstance()->bootstrap();
         }
 
@@ -308,24 +293,6 @@ class WidgetFramework_Listener
 
     public static function load_class_view($class, array &$extend)
     {
-        static $extended1 = false;
-        static $extended2 = false;
-
-        if (defined('WIDGET_FRAMEWORK_LOADED')
-            && !empty($class)
-        ) {
-            // check for empty($class) to avoid a bug with XenForo 1.2.0
-            // http://xenforo.com/community/threads/57064/
-
-            if (empty($extended1)) {
-                $extend[] = 'WidgetFramework_XenForo_View1';
-                $extended1 = $class;
-            } elseif (empty($extended2)) {
-                $extend[] = 'WidgetFramework_XenForo_View2';
-                $extended2 = $class;
-            }
-        }
-
         if ($class === 'XenForo_ViewAdmin_StyleProperty_List') {
             $extend[] = 'WidgetFramework_' . $class;
         }

library/WidgetFramework/Core.php
@@ -31,7 +31,6 @@ class WidgetFramework_Core
     const PARAM_GROUP_ID = '_WidgetFramework_groupId';
     const PARAM_PARENT_GROUP_ID = '_WidgetFramework_parentGroupId';
     const PARAM_PARENT_TEMPLATE = '_WidgetFramework_parentTemplate';
-    const PARAM_VIEW_OBJECT = '_WidgetFramework_viewObj';
     const PARAM_TEMPLATE_OBJECTS = '_WidgetFramework_templateObjects';
     const PARAM_CURRENT_WIDGET_ID = '_WidgetFramework_currentWidgetId';
 

library/WidgetFramework/WidgetRenderer.php
@@ -11,7 +11,6 @@ abstract class WidgetFramework_WidgetRenderer
     const PARAM_GROUP_NAME = '_WidgetFramework_groupId';
     const PARAM_PARENT_GROUP_NAME = '_WidgetFramework_parentGroupId';
     const PARAM_PARENT_TEMPLATE = '_WidgetFramework_parentTemplate';
-    const PARAM_VIEW_OBJECT = '_WidgetFramework_viewObj';
     const PARAM_TEMPLATE_OBJECTS = '_WidgetFramework_templateObjects';
     // please use constants from WidgetFramework_Core from now on
 
@@ -912,26 +911,13 @@ abstract class WidgetFramework_WidgetRenderer
 
     public static function getViewObject(array $params, XenForo_Template_Abstract $templateObj)
     {
-        if (isset($params[WidgetFramework_Core::PARAM_VIEW_OBJECT])) {
-            return $params[WidgetFramework_Core::PARAM_VIEW_OBJECT];
-        }
-
-        $viewObj = $templateObj->getParam(WidgetFramework_Core::PARAM_VIEW_OBJECT);
-        if (!empty($viewObj)) {
-            return $viewObj;
-        }
-
-        if (empty(self::$_pseudoViewObj)) {
-            if (!empty(WidgetFramework_Listener::$fc)
-                && !empty(WidgetFramework_Listener::$viewRenderer)
-            ) {
-                if (WidgetFramework_Listener::$viewRenderer instanceof XenForo_ViewRenderer_HtmlPublic) {
-                    self::$_pseudoViewObj = new XenForo_ViewPublic_Base(
-                        WidgetFramework_Listener::$viewRenderer,
-                        WidgetFramework_Listener::$fc->getResponse()
-                    );
-                }
-            }
+        if (empty(self::$_pseudoViewObj)
+            && WidgetFramework_Listener::$viewRenderer !== null
+        ) {
+            self::$_pseudoViewObj = new XenForo_ViewPublic_Base(
+                WidgetFramework_Listener::$viewRenderer,
+                XenForo_Application::getFc()->getResponse()
+            );
         }
 
         if (!empty(self::$_pseudoViewObj)) {

library/WidgetFramework/WidgetRenderer/Template.php
@@ -122,10 +122,6 @@ class WidgetFramework_WidgetRenderer_Template extends WidgetFramework_WidgetRend
 
     protected function _dispatch($controllerName, $controllerAction)
     {
-        if (empty(WidgetFramework_Listener::$fc)) {
-            return null;
-        }
-
         $routeMatch = new XenForo_RouteMatch($controllerName, $controllerAction);
 
         // do not use `html` response type to avoid being redirected by
@@ -133,7 +129,7 @@ class WidgetFramework_WidgetRenderer_Template extends WidgetFramework_WidgetRend
         $routeMatch->setResponseType(get_class($this));
 
         try {
-            $controllerResponse = WidgetFramework_Listener::$fc->dispatch($routeMatch);
+            $controllerResponse = XenForo_Application::getFc()->dispatch($routeMatch);
 
             return $controllerResponse;
         } catch (Exception $e) {
@@ -169,18 +165,17 @@ class WidgetFramework_WidgetRenderer_Template extends WidgetFramework_WidgetRend
 
     protected function _renderView(array $widget, XenForo_ControllerResponse_Abstract $controllerResponse)
     {
-        if (empty(WidgetFramework_Listener::$fc)) {
-            return null;
-        }
+        $fc = XenForo_Application::getFc();
+        $dependencies = $fc->getDependencies();
 
-        $response = WidgetFramework_Listener::$fc->getResponse();
-        $request = WidgetFramework_Listener::$fc->getRequest();
-        $viewRenderer = WidgetFramework_Listener::$fc->getDependencies()->getViewRenderer($response, 'html', $request);
+        $response = $fc->getResponse();
+        $request = $fc->getRequest();
+        $viewRenderer = $dependencies->getViewRenderer($response, 'html', $request);
+
         $viewRenderer->setNeedsContainer(false);
+        $renderedView = strval($fc->renderView($controllerResponse, $viewRenderer));
 
-        $renderedView = strval(WidgetFramework_Listener::$fc->renderView($controllerResponse, $viewRenderer));
-
-        self::$_extraContainerDatas[$widget['widget_id']] = WidgetFramework_Listener::$fc->getDependencies()->getExtraContainerData();
+        self::$_extraContainerDatas[$widget['widget_id']] = $dependencies->getExtraContainerData();
 
         return $renderedView;
     }

library/WidgetFramework/XenForo/View1.php
@@ -1,14 +1,0 @@
-<?php
-
-class WidgetFramework_XenForo_View1 extends XFCP_WidgetFramework_XenForo_View1
-{
-    public function getParams()
-    {
-        $params = parent::getParams();
-
-        $params[WidgetFramework_Core::PARAM_VIEW_OBJECT] = $this;
-
-        return $params;
-    }
-
-}

library/WidgetFramework/XenForo/View2.php
@@ -1,14 +1,0 @@
-<?php
-
-class WidgetFramework_XenForo_View2 extends XFCP_WidgetFramework_XenForo_View2
-{
-    public function getParams()
-    {
-        $params = parent::getParams();
-
-        $params[WidgetFramework_Core::PARAM_VIEW_OBJECT] = $this;
-
-        return $params;
-    }
-
-}

commit 78dbd7eb4353aa55a4e20e5f0264b73c29c51940

Fix bug loading widget via ajax caused by 9c6e0cf

library/WidgetFramework/Listener.php
@@ -231,9 +231,7 @@ class WidgetFramework_Listener
         /** @noinspection PhpUnusedParameterInspection */
         array &$containerParams
     ) {
-        if (self::$viewRenderer === null
-            && $viewRenderer instanceof XenForo_ViewRenderer_HtmlPublic
-        ) {
+        if ($fc->getDependencies() instanceof XenForo_Dependencies_Public) {
             self::$viewRenderer = $viewRenderer;
             WidgetFramework_Core::getInstance()->bootstrap();
         }

library/WidgetFramework/ViewPublic/Widget/Ajax.php
@@ -25,7 +25,6 @@ class WidgetFramework_ViewPublic_Widget_Ajax extends XenForo_ViewPublic_Base
 
         $widgets = array($widget['widget_id'] => $widget);
         $core = WidgetFramework_Core::getInstance();
-//        $core->removeAllWidgets();
         $core->addWidgets($widgets);
 
         $_REQUEST['_getRender'] = 1;